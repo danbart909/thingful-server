72370fb3df32cc4a2933d92aa3291047afc9cfb2
Dan Bartlett <danbart909@gmail.com>
2020-03-03 21:24:34 -0500

    maiden voyage

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  PORT: process.env.PORT || 9090,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || 'postgresql://postgres:401923@localhost/thingful',
+  TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://postgres:401923@localhost/thingful-test',
+}
