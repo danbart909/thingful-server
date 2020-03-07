2142599b28837558179cb7b2d728d952b9bc905d
Dan Bartlett <danbart909@gmail.com>
2020-03-07 10:53:46 -0500

    .

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -4,11 +4,25 @@ const AuthService = {
       .where({ user_name })
       .first()
   },
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash)
+  },
   parseBasicToken(token) {
     return Buffer
       .from(token, 'base64')
       .toString()
       .split(':')
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      algorithm: 'HS256',
+    })
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ['HS256'],
+    })
   },
 }
 
--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://postgres:401923@localhost/thingful',
   TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://postgres:401923@localhost/thingful-test',
+  JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret'
 }
