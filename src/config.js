202c50f83c72af4f18e3bb2faea45035f7e80ee2
Dan Bartlett <danbart909@gmail.com>
2020-03-12 18:53:58 -0400

    .

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://postgres:401923@localhost/thingful',
   TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://postgres:401923@localhost/thingful-test',
-  JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret'
+  JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
